ea480c7dc54e27e70fe59e57c9a7769b0097b0aa
DBM <diegobarmor42@gmail.com>
2025-03-28 13:30:37 +0100

    testing with colors

---
+++ tests/colors.py
@@ -0,0 +1,43 @@
+import json
+import curses
+
+
+# 0: curses.COLOR_BLACK
+# 1: curses.COLOR_RED
+# 2: curses.COLOR_GREEN
+# 3: curses.COLOR_YELLOW
+# 4: curses.COLOR_BLUE
+# 5: curses.COLOR_MAGENTA
+# 6: curses.COLOR_CYAN
+# 7: curses.COLOR_WHITE
+
+
+def main(stdscr: curses.window):
+    path_palette = "tests/palette.json"
+    with open(path_palette, 'r') as file:
+        palette = json.load(file)
+
+    # palette should be of 248 colors max, the first 8 are to be reserved to the default curses values
+
+    # n = 256
+    # c = 1000 // n
+    char = -1
+    while char != curses.KEY_F1:
+        _,w = stdscr.getmaxyx()
+        stdscr.erase()
+        for i,color in enumerate(palette):
+            y, x = divmod(i*4, w)
+
+            if i >= 8:
+                curses.init_color(i, *color)
+            # curses.init_color(i, 0, 0, i*c)
+
+            if i:
+                curses.init_pair(i, curses.COLOR_BLACK, i)
+
+            # stdscr.addstr(y, x, f"{i:03}")
+            stdscr.addstr(y, x, f"{i:03}", curses.color_pair(i))
+
+        char = stdscr.getch()
+
+curses.wrapper(main)
---
+++ tests/gen_palette.py
@@ -0,0 +1,27 @@
+import json
+
+def hex2rgb(hex_str):
+    hex_str = hex_str.lstrip("#")
+    return tuple(int(hex_str[i:i+2], base = 16) for i in (0, 2, 4))
+
+def rgb_curses(rgb):
+    return tuple(int(1000 * c / 255) for c in rgb)
+
+def merge_colors(r,g,b):
+    return r[0], g[1], b[2]
+
+hex_colors = dict( # https://en.wikipedia.org/wiki/List_of_software_palettes#6-8-5_levels_RGB
+    reds   = ["#000000", "#330000", "#660000", "#990000", "#CC0000", "#FF0000"],
+    greens = ["#000000", "#002400", "#004900", "#006D00", "#009200", "#00B600", "#00DB00", "#00FF00"],
+    blues  = ["#000000", "#000040", "#000080", "#0000BF", "#0000FF"],
+)
+
+rgb_colors = {k: [rgb_curses(hex2rgb(c)) for c in v] for k,v in hex_colors.items()}
+
+palette = [merge_colors(r,g,b) for r in rgb_colors["reds"] for g in rgb_colors["greens"] for b in rgb_colors["blues"]]
+
+print(palette)
+
+path_palette = "tests/palette.json"
+with open(path_palette, 'w') as file:
+    json.dump(palette, file)
